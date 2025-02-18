c7fe6339fb5247ffa359d433d6e77327b49a6949
ghost <ghost@unknown.com>
2025-02-18 14:48:05 +0100

    Macroquad boilerplate

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, world!");
+use macroquad::prelude::*;
+
+#[macroquad::main("SpaceInvaders")]
+async fn main() {
+    loop {
+        clear_background(BLACK);
+
+        // Code...
+
+        next_frame().await
+    }
 }
